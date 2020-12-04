0398d94c6edfa34d9b90196a2865e5e47221a831
Nitesh Karna <karnanitace16@gmail.com>
2020-12-04 09:36:39 +0545

    Completed basic layout design

---
+++ dist/js/query.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  // jQuery methods go here...
+  $(".inner").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+  });
+});
